535941f402b9a13a7967bc6def200a2e4eedfd00
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-22 16:44:25 +0100

    Update Nav

---
+++ assets/scripts.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("nav")
+const navButton = document.querySelector("nav div button")
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY < 500) {
+        nav.style.backgroundColor = "#ffc017"
+        navButton.style.backgroundColor = "#191919"
+    } else {
+        nav.style.backgroundColor = "white"
+        navButton.style.backgroundColor = "#538724"
+    }
+});
+
+
